ade43bd0b94c62719288e0061e49b84db493409e
xuyongzhi <buaaxyz@yeah.net>
2019-04-06 17:30:46 +1100

    change config

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=0
 #export CUDA_LAUNCH_BLOCKING=1 
 TEST='--skip-test'
 #TEST='--only-test'
-#CONFIG_FILE='nms_bs1_difyaw.yaml'
-CONFIG_FILE='nms_bs1_sinyaw.yaml'
+CONFIG_FILE='nms_bs1_difyaw.yaml'
+#CONFIG_FILE='nms_bs1_sinyaw.yaml'
 ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
 
