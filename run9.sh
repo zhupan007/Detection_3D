405ab01dc9791f1c313198733d383f844325ef5f
xuyongzhi <buaaxyz@yeah.net>
2019-04-17 10:51:47 +1000

    ju

--- run9.sh
+++ run9.sh
@@ -1,9 +1,9 @@
 export PYTHONPATH=$PWD
-#export CUDA_VISIBLE_DEVICES=0
-export CUDA_LAUNCH_BLOCKING=1 
+export CUDA_VISIBLE_DEVICES=0
+#export CUDA_LAUNCH_BLOCKING=1 
 TEST='--skip-test'
 #TEST='--only-test'
-CONFIG_FILE='dlr_bs1.yaml'
+CONFIG_FILE='dlr_bs9.yaml'
 
 ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
 
