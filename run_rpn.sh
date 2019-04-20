f59e7f99637c76909daef5ddbcdfa57c9c5f89f6
xuyongzhi <buaaxyz@yeah.net>
2019-04-20 08:00:26 -0400

    test the settings at 19 April

--- run_rpn.sh
+++ run_rpn.sh
@@ -3,7 +3,7 @@ export PYTHONPATH=$PWD
 #export CUDA_LAUNCH_BLOCKING=1 
 TEST='--skip-test'
 #TEST='--only-test'
-CONFIG_FILE='dlr_bs1.yaml'
+CONFIG_FILE='dlr_bs1_rpn.yaml'
 
 ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
 
