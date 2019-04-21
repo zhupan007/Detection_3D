b080282b2a3470731bbee19ef7c4c95fcb9913cd
xuyongzhi <buaaxyz@yeah.net>
2019-04-21 21:42:31 +1000

    smaller learning rate to avoid nan loss

--- run.sh
+++ run.sh
@@ -5,8 +5,7 @@ export CUDA_VISIBLE_DEVICES=1
 TEST='--skip-test'
 #TEST='--only-test'
 
-CONFIG_FILE='dlr_bs1.yaml'
-#CONFIG_FILE='fpn3_bs1.yaml'
+CONFIG_FILE='fpn3_bs1.yaml'
 
 ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
 
