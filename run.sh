28ef43b3dbd3083c41daceb217551706e4cbdba0
xuyongzhi <buaaxyz@yeah.net>
2019-05-17 21:45:46 +1000

    add config for wall only

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 export PYTHONPATH=$PWD
 #export CUDA_LAUNCH_BLOCKING=1 
-export CUDA_VISIBLE_DEVICES=1
+#export CUDA_VISIBLE_DEVICES=1
 
 TEST='--skip-test'
 #TEST='--only-test'
 
-CONFIG_FILE='fpn432_3d1_bs1_4c.yaml'
+CONFIG_FILE='fpn432_2d_bs1_wall.yaml'
+#CONFIG_FILE='fpn432_2d_bs1_4c_noaugroi_wall.yaml'
 
 ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
 
