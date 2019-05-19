86ec70b8eaab8ad19cdb42efac5c5291cc6ed2e1
xuyongzhi <buaaxyz@yeah.net>
2019-05-19 11:28:50 +1000

    roi on 153 example is very bad, totally no convergence for both wall and 3 classess

--- run_1.sh
+++ run_1.sh
@@ -6,6 +6,7 @@ TEST='--skip-test'
 #TEST='--only-test'
 
 CONFIG_FILE='fpn432_3d1_bs1_4c.yaml'
+CONFIG_FILE='fpn432_3d1_bs1_2c.yaml'
 
 ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
 
