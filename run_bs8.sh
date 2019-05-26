3aa6b9ed6efca11902ab0146ada45a1666f9f211
xuyongzhi <buaaxyz@yeah.net>
2019-05-26 11:20:00 +1000

    add config

---
+++ run_bs8.sh
@@ -0,0 +1,12 @@
+export PYTHONPATH=$PWD
+#export CUDA_LAUNCH_BLOCKING=1 
+export CUDA_VISIBLE_DEVICES=1
+
+TEST='--skip-test'
+#TEST='--only-test'
+
+CONFIG_FILE='fpn432_2d_bs1_wall.yaml'
+CONFIG_FILE='fpn432_3d1_bs8_4c.yaml'
+
+ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
+
