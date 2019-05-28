13a24eb278fc10bbb62d0317b5889d9229c6f537
xuyongzhi <buaaxyz@yeah.net>
2019-05-28 11:32:25 +1000

    enable using pytorch 1.0.0

--- maskrcnn_benchmark/utils/model_zoo.py
+++ maskrcnn_benchmark/utils/model_zoo.py
@@ -4,7 +4,7 @@ import sys
 
 from torch.hub import _download_url_to_file
 from torch.hub import urlparse
-from torch.hub import HASH_REGEX
+#from torch.hub import HASH_REGEX
 
 from maskrcnn_benchmark.utils.comm import is_main_process
 from maskrcnn_benchmark.utils.comm import synchronize
