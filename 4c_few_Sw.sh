c659b96b6452588441a5eaa360998a5ea7772700
xuyongzhi <buaaxyz@yeah.net>
2019-06-08 20:32:42 +1000

    add config

---
+++ 4c_few_Sw.sh
@@ -0,0 +1,11 @@
+export PYTHONPATH=$PWD
+#export CUDA_LAUNCH_BLOCKING=1 
+#export CUDA_VISIBLE_DEVICES=1
+
+TEST='--skip-test'
+#TEST='--only-test'
+
+CONFIG_FILE='fpn432_3d1_bs1_4c_lr10_2S_Sw.yaml'
+
+ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
+
