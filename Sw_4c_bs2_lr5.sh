d14196fa1e45502ac819e74cffb9af0e71c3f306
xuyongzhi <buaaxyz@yeah.net>
2019-07-19 18:34:54 +1000

    fix config

---
+++ Sw_4c_bs2_lr5.sh
@@ -0,0 +1,11 @@
+export PYTHONPATH=$PWD
+#export CUDA_LAUNCH_BLOCKING=1 
+export CUDA_VISIBLE_DEVICES=1
+
+#TEST='--skip-test'
+#TEST='--only-test' 
+
+CONFIG_FILE='ROI2D_Sw4c_fpn432_bs2_lr5.yaml'
+
+ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
+
