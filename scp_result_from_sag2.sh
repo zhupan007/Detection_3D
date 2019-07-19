1281c0f0c3907d2ec7af7bf1d0b6dcc1c3c0792d
xuyongzhi <buaaxyz@yeah.net>
2019-07-19 15:58:08 +1000

    update configs

--- scp_result_from_sag2.sh
+++ scp_result_from_sag2.sh
@@ -1,8 +1,9 @@
 
 # sage2 3d
 #scp -r z@129.94.237.104:/home/z/Research/Detection_3D/res_* ./
+scp -r ./res_*  z@129.94.237.104:/home/z/Research/Detection_3D/
 
 
 # sage1 Kc9
-scp -r z@149.171.144.165:/home/z/Research/Detection_3D/res_* ./
+#scp -r z@149.171.144.165:/home/z/Research/Detection_3D/res_* ./
 
