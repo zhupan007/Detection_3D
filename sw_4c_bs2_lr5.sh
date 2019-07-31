64532843b61705f1930480c205467363bfde6788
xuyongzhi <buaaxyz@yeah.net>
2019-07-31 10:44:58 +1000

    rename config

--- sw_4c_bs2_lr5.sh
+++ sw_4c_bs2_lr5.sh
@@ -5,8 +5,7 @@ export CUDA_VISIBLE_DEVICES=1
 #TEST='--skip-test'
 #TEST='--only-test' 
 
-#CONFIG_FILE='Sw4c_fpn432_bs2_lr5.yaml'
-CONFIG_FILE='ROI2D_Sw4c_fpn432_bs2_lr5.yaml'
+CONFIG_FILE='sw4c_fpn432_bs2_lr5.yaml'
 
 ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
 
