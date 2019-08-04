5b60ad7f441d8e2a23390fef69c83bfd2e726704
xuyongzhi <buaaxyz@yeah.net>
2019-08-04 11:33:46 +1000

    clean

--- sw_4c_fpn4321_bs1_lr10.sh
+++ sw_4c_fpn4321_bs1_lr10.sh
@@ -5,7 +5,7 @@ export PYTHONPATH=$PWD
 #TEST='--skip-test'
 #TEST='--only-test' 
 
-CONFIG_FILE='6c/5c_fpn4321_bs1_lr5_rpn.yaml'
+CONFIG_FILE='sw4c/sw4c_fpn4321_bs1_lr10.yaml'
 
 ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
 
