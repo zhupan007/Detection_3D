e5ed810d39b9117682979d331620e5675b950621
xuyongzhi <buaaxyz@yeah.net>
2019-08-10 10:05:28 +1000

    ju

--- sw_6c_bs1_lr10.sh
+++ sw_6c_bs1_lr10.sh
@@ -5,8 +5,8 @@ export PYTHONPATH=$PWD
 #TEST='--skip-test'
 #TEST='--only-test' 
 
-#CONFIG_FILE='sw4c_fpn432_bs1_lr5.yaml'
-CONFIG_FILE='t1_sw6c/sw6c_fpn4321_bs1_lr10.yaml'
+#CONFIG_FILE='t1_sw6c/sw6c_fpn4321_bs1_lr10.yaml'
+CONFIG_FILE='t1_sw6c/sw6c_fpn4321_p4_bs1_lr10.yaml'
 
 ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
 
