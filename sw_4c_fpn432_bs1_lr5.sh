fcbc9ff4e634df681f33b7737f3b697f9748eb22
xuyongzhi <buaaxyz@yeah.net>
2019-08-14 19:49:25 +1000

    edit config

--- sw_4c_fpn432_bs1_lr5.sh
+++ sw_4c_fpn432_bs1_lr5.sh
@@ -5,7 +5,8 @@ export PYTHONPATH=$PWD
 #TEST='--skip-test'
 #TEST='--only-test' 
 
-CONFIG_FILE='sw4c/sw4c_fpn432_bs1_lr10.yaml'
+CONFIG_FILE='t1_sw4c/sw4c_fpn432_bs1_lr10.yaml'
+#CONFIG_FILE='sw4c/sw4c_fpn432_bs1_lr5.yaml'
 
 ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
 
