88b48a27a9261de3d6ed8ea89db60f9e37151b50
xuyongzhi <buaaxyz@yeah.net>
2019-08-25 07:34:31 +1000

    update Small Data configs

--- sw_4c_fpn432_bs1_lr2.sh
+++ sw_4c_fpn432_bs1_lr2.sh
@@ -5,8 +5,8 @@ export CUDA_VISIBLE_DEVICES=1
 #TEST='--skip-test'
 #TEST='--only-test' 
 
-CONFIG_FILE='SeW4c/SeW4c_Fpn432_bs1_lr2.yaml'
-#CONFIG_FILE='SeW4c/SeW4c_Fpn432_bs1_lr2_SD.yaml'
+#CONFIG_FILE='SeW4c/SeW4c_Fpn432_bs1_lr2.yaml'
+CONFIG_FILE='SeW4c/SeW4c_Fpn432_bs1_lr5_SD.yaml'
 
 ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
 
