dea5353b5d7ae71884706a5a825ba989440138d3
xuyongzhi <buaaxyz@yeah.net>
2019-11-07 15:07:07 +1100

    new config

--- wall.sh
+++ wall.sh
@@ -1,14 +1,14 @@
 export PYTHONPATH=$PWD
 #export CUDA_LAUNCH_BLOCKING=1 
-export CUDA_VISIBLE_DEVICES=1
+#export CUDA_VISIBLE_DEVICES=1
 
 #TEST='--skip-test'
 #TEST='--only-test' 
 
 
 
-CONFIG_FILE='3G6c/wall_Fpn4321_bs1_lr20_SD.yaml'
 CONFIG_FILE='Wall/wall_Fpn432_BEV_bs1_lr20_SD.yaml'
+CONFIG_FILE='Wall/wall_Fpn432_BEV_bs1_lr20.yaml'
 
 ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
 
