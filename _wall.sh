14436ae921d317e5f6250af8060f22254d6d23c3
xuyongzhi <buaaxyz@yeah.net>
2019-11-18 13:19:36 +1100

    new config

--- _wall.sh
+++ _wall.sh
@@ -7,11 +7,11 @@ export PYTHONPATH=$PWD
 
 
 #CONFIG_FILE='walls/wall_Fpn4321_bs1_lr5.yaml'
-#CONFIG_FILE='walls/wall_Fpn4321_bs1_lr5_corsem.yaml'
+CONFIG_FILE='walls/wall_Fpn4321_bs1_lr5_corsem.yaml'
 #CONFIG_FILE='walls/wall_Fpn4321_bs1_lr5_Rpn.yaml'
 #CONFIG_FILE='walls/wall_Fpn4321_bs1_lr5_CB.yaml'
 
-CONFIG_FILE='walls/SD_wall_Fpn4321_bs1_lr20.yaml'
+#CONFIG_FILE='walls/SD_wall_Fpn4321_bs1_lr20.yaml'
 #CONFIG_FILE='walls/SD_wall_Fpn4321_bs1_lr20_corsem.yaml'
 #CONFIG_FILE='walls/SD_wall_Fpn4321_bs1_lr20_Rpn.yaml'
 #CONFIG_FILE='walls/SD_wall_Fpn4321_bs1_lr20_CB.yaml'
