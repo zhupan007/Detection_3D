7ae097a70adaf65f82313096da78c762165559e1
xuyongzhi <buaaxyz@yeah.net>
2019-11-18 11:03:30 +1100

    organize configs

--- wall.sh
+++ wall.sh
@@ -1,16 +1,16 @@
 export PYTHONPATH=$PWD
 #export CUDA_LAUNCH_BLOCKING=1 
-export CUDA_VISIBLE_DEVICES=1
+#export CUDA_VISIBLE_DEVICES=1
 
 #TEST='--skip-test'
 #TEST='--only-test' 
 
 
 
-#CONFIG_FILE='Wall/wall_Fpn4321_bs1_lr20_SD.yaml'
-CONFIG_FILE='Wall/wall_Fpn4321_bs1_lr20_corsem_SD_RPN.yaml'
+CONFIG_FILE='Wall/wall_Fpn4321_bs1_lr20_SD.yaml'
+#CONFIG_FILE='Wall/wall_Fpn4321_bs1_lr20_corsem_SD_RPN.yaml'
 #CONFIG_FILE='Wall/wall_Fpn4321_bs1_lr20_corsem_SD.yaml'
-#CONFIG_FILE='Wall/wall_Fpn4321_bs1_lr20_SD_CB.yaml'
+CONFIG_FILE='Wall/wall_Fpn4321_bs1_lr20_SD_CB.yaml'
 #CONFIG_FILE='Wall/wall_Fpn4321_bs1_lr5.yaml'
 #CONFIG_FILE='Wall/wall_Fpn4321_bs1_lr5_RPN.yaml'
 
