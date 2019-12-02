ff3871aba0209b6ea0a468ce661e769f0b80e8cc
xuyongzhi <buaaxyz@yeah.net>
2019-12-02 21:01:26 +1100

    fix config name

--- _3g6c.sh
+++ _3g6c.sh
@@ -9,12 +9,12 @@ export CUDA_VISIBLE_DEVICES=1
 #CONFIG_FILE='3g6cs/3g6c_Fpn4321_bs1_lr5.yaml'
 #CONFIG_FILE='3g6cs/3g6c_Fpn4321_bs1_lr5_corsem.yaml'
 #CONFIG_FILE='3g6cs/3g6c_Fpn4321_bs1_lr5_Rpn.yaml'
-#CONFIG_FILE='3g6cs/3g6c_Fpn4321_bs1_lr5_CB.yaml'
+CONFIG_FILE='3g6cs/3g6c_Fpn4321_bs1_lr5_CB.yaml'
 
 #CONFIG_FILE='3g6cs/SD_3g6c_Fpn4321_bs1_lr20.yaml'
 #CONFIG_FILE='3g6cs/SD_3g6c_Fpn4321_bs1_lr20_corsem.yaml'
 #CONFIG_FILE='3g6cs/SD_3g6c_Fpn4321_bs1_lr20_Rpn.yaml'
-CONFIG_FILE='3g6cs/SD_3g6c_Fpn4321_bs1_lr20_CB.yaml'
+#CONFIG_FILE='3g6cs/SD_3g6c_Fpn4321_bs1_lr20_CB.yaml'
 
 ipython tools/train_net_sparse3d.py -- --config-file "configs/$CONFIG_FILE"  $TEST
 
